b7f2fb51ebd8d6af3282d58f41dd2d1b07d0bc10
hamabehayato <be_dash1122@icloud.com>
2021-05-03 21:46:51 +0900

    #15 Userモデルのバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true, length: { maximum: 10 }
+    validates :last_name, presence: true, length: { maximum: 10 }
+    validates :zipcode, presence: true, format: {with: /^\d{7}$/}
+    validates :prefecture, presence: true, length: { maximum: 5 }
+    validates :municipality, presence: true, length: { maximum: 10 }
+    validates :address, presence: true, length: { maximum: 10 }
+    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+    validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX }
+    validates :password, presence: true, length: { minimum: 6, maximum: 15 }
+
     belongs_to :user_classification
 end
