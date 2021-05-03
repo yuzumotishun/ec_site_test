c7a276128f64b76dcbae861d3c3a839dc2906628
hamabehayato <be_dash1122@icloud.com>
2021-05-03 20:34:32 +0900

    #17 product関連のseed作成&タイポ修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :purchases, dependent: :destroy
   belongs_to :category
-  belongs_to :sale_statuse
-  belongs_to :product_statuse
+  belongs_to :sale_status
+  belongs_to :product_status
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+category = Category.create!(
+  category_name: "food"
+)
+
+sale_status = SaleStatus.create!(
+  sale_status_name: "sale"
+)
+
+product_status = ProductStatus.create!(
+  product_status_name: "on-sale"
+)
+
+product = Product.create!(
+  product_name: "sample_product",
+  price: 1000,
+  description: "サンプル製品です。",
+  delete_flag: false,
+  category_id: category.id,
+  sale_status_id: sale_status.id,
+  product_status_id: product_status.id
+)
+
+Purchase.create!(
+  purchase_price: 1000,
+  purchase_quantity: 1,
+  purchase_company: "架空社",
+  product_id: product.id
+)
