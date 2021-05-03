85bc0b7aabdfd616ca27638d23501e0ad2a113f7
hamabehayato <be_dash1122@icloud.com>
2021-05-03 10:59:45 +0900

    #13 必要なカラムを追加

--- db/migrate/20210502133409_create_sale_statuses.rb
+++ db/migrate/20210502133409_create_sale_statuses.rb
@@ -1,6 +1,7 @@
 class CreateSaleStatuses < ActiveRecord::Migration[6.0]
   def change
     create_table :sale_statuses do |t|
+      t.string :sale_status_name, limit: 32
 
       t.timestamps
     end
--- db/migrate/20210502133458_create_product_statuses.rb
+++ db/migrate/20210502133458_create_product_statuses.rb
@@ -1,6 +1,7 @@
 class CreateProductStatuses < ActiveRecord::Migration[6.0]
   def change
     create_table :product_statuses do |t|
+      t.string :product_status_name, limit: 32
 
       t.timestamps
     end
