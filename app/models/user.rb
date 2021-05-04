ecf191b08cb1b16f8b7eae350b0eca28e63d2e1d
hamabehayato <be_dash1122@icloud.com>
2021-05-04 14:48:43 +0900

    #15 userモデルのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
     #ハイフンなし、7桁の整数
-    VALID_ZIPCODE_REGEX = /^\d{7}$/
+    VALID_ZIPCODE_REGEX = /\A\d{7}\z/
     #メールアドレス
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     #電話番号 ハイフンなし、10桁 または 11桁の整数
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
     validates :first_name, presence: true, length: { maximum: 10 }
     validates :last_name, presence: true, length: { maximum: 10 }
-    validates :zipcode, presence: true, format: {with: VALID_ZIPCODE_REGEX}
+    validates :zipcode, presence: true, format: {with: VALID_ZIPCODE_REGEX }
     validates :prefecture, presence: true, length: { maximum: 5 }
     validates :municipality, presence: true, length: { maximum: 10 }
     validates :address, presence: true, length: { maximum: 10 }
