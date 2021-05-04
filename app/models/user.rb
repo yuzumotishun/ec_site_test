934744600cb3f14d98f573e88a7ee86eb878fca6
hamabehayato <be_dash1122@icloud.com>
2021-05-04 17:59:30 +0900

    #15 validates phone_number修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
 
     #ハイフンなし、7桁の整数
-    VALID_ZIPCODE_REGEX = /\A\d{7}\z/
+    VALID_ZIPCODE_REGEX = /\A\d{3}[-]\d{4}\z/
     #メールアドレス
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     #電話番号 ハイフンなし、10桁 または 11桁の整数
-    VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+    VALID_PHONE_REGEX = /\A0\d{1,4}-\d{1,4}-\d{3,4}\z/
 
     validates :first_name, presence: true, length: { maximum: 10 }
     validates :last_name, presence: true, length: { maximum: 10 }
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     validates :municipality, presence: true, length: { maximum: 10 }
     validates :address, presence: true, length: { maximum: 10 }
     validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-    validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX }
+    validates :phone_number, presence: true, length: { maximum: 15 }, format: { with: VALID_PHONE_REGEX }
     validates :password, presence: true, length: { minimum: 6, maximum: 15 }
 
     belongs_to :user_classification
