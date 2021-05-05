7853bb1812c409a6fbe5884603d491f289df5746
hamabehayato <be_dash1122@icloud.com>
2021-05-05 21:06:16 +0900

    #17 userモデルにhas_many :products を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ApplicationRecord
+
+    #ハイフンあり、7桁の整数
+    VALID_ZIPCODE_REGEX = /\A\d{3}[-]\d{4}\z/
+    #メールアドレス
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    #電話番号 ハイフンあり、整数
+    VALID_PHONE_REGEX = /\A[0-9-]+\z/
+
+    validates :first_name, presence: true, length: { maximum: 10 }
+    validates :last_name, presence: true, length: { maximum: 10 }
+    validates :zipcode, presence: true, format: {with: VALID_ZIPCODE_REGEX }
+    validates :prefecture, presence: true, length: { maximum: 5 }
+    validates :municipality, presence: true, length: { maximum: 10 }
+    validates :address, presence: true, length: { maximum: 10 }
+    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+    validates :phone_number, presence: true, length: { maximum: 15 }, format: { with: VALID_PHONE_REGEX }
+    validates :password, presence: true, length: { minimum: 6, maximum: 15 }
+
+    has_many :products
     belongs_to :user_classification
+    has_secure_password
 end
