e1f0b0ad9d1c1659173d58e9a08e79ecb6bc0c8c
hamabehayato <be_dash1122@icloud.com>
2021-05-07 23:28:54 +0900

    #28 不要なファイルを削除

--- test/controllers/products_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test "should get product_details" do
-    get products_product_details_url
-    assert_response :success
-  end
-end
