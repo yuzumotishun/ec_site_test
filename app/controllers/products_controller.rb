67f5436d25d4d34a7f69369e38d992c20341f59f
hamabehayato <be_dash1122@icloud.com>
2021-05-07 20:29:51 +0900

    #28 not_findページをshow.html に合わせる形で修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def show
-    if !Product.exists?(params[:id])
+    unless Product.exists?(params[:id])
       render products_not_find_path
     end
   end
