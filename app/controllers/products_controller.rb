ce8029902afd2846a0e298d9bb5c89b4ee59d64c
hamabehayato <be_dash1122@icloud.com>
2021-05-08 18:08:20 +0900

    #28 不要なインスタンス変数削除＆動的表示の方法を修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,5 @@
 class ProductsController < ApplicationController
   def show
-    @product = Product.find(params[:id])
-
-    ### header 統一化後、削除して良い
-    @user = User.find(params[:id])
+    @product = Product.find_by(id: params[:id])
   end
 end
