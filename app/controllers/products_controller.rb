1a543982282c86dc569a8bcaad70e554c9d99a0d
hamabehayato <be_dash1122@icloud.com>
2021-05-08 17:52:51 +0900

    #28 不要なインスタンス変数 @product_find_check を削除

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
   def show
-    @product_find_check = Product.find_by(id: params[:id])
     @product = Product.find(params[:id])
 
     ### header 統一化後、削除して良い
