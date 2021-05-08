fca8fe44297fbaf35d1f38ac0b0a4ed0817a00bf
hamabehayato <be_dash1122@icloud.com>
2021-05-08 17:04:29 +0900

    #28 商品の、「名前」「カテゴリ名」「説明」「値段」、user の名前、を動的な表示に変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
   def show
-    @product = Product.find_by(id: params[:id])
+    @product_find_check = Product.find_by(id: params[:id])
+    @product = Product.find(params[:id])
+
+    ### header 統一化後、削除して良い
+    @user = User.find(params[:id])
   end
 end
