94f0dabacdbbe85259c59ab8f7bd051531dfcff7
hamabehayato <be_dash1122@icloud.com>
2021-05-12 15:42:21 +0900

    #33 ユーザ情報の更新機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:password, :last_name, :first_name, :zipcode, :prefecture, :municipality, :address, :apartments, :email, :phone_number)
+  end
 end
