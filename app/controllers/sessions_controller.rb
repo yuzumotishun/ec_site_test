76fc01d1b140cd024aace2717bfc6f4fe24b247f
hamabehayato <be_dash1122@icloud.com>
2021-05-16 07:49:41 +0900

    #36 ログイン機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      log_in user
+      flash[:success] = "ログインしました。"
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = "ログインできませんでした。"
+      render :new
+    end
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/login' => 'sessions#new'
+  post   'login'   => 'sessions#create'
   resources :products
   resources :users
 end
