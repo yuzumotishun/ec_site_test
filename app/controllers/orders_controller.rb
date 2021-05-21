543162bc669a171cad868c39e566245d95ba60f1
hamabehayato <be_dash1122@icloud.com>
2021-05-21 18:09:37 +0900

    #30 注文詳細画面内、Noにあたる部分に該当する要素がなかったため、新たに@item_nuber を作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   def show
     @sum_price = 0
+    @item_number = 1
     @orders = Order.find_by(id: params[:id])
     @shipment_status = OrderDetail.find_by(id: params[:id]).shipment_status.shipment_status_name
   end
