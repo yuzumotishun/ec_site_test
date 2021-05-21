519fb3f204d0cc9090cdb1b990a585780c799783
hamabehayato <be_dash1122@icloud.com>
2021-05-21 18:06:25 +0900

    #30 @orders.order_details.shipment_status.shipment_status_name で注文状態を呼び出せなかったので、新たに@shipment_status を作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,6 @@ class OrdersController < ApplicationController
   def show
     @sum_price = 0
     @orders = Order.find_by(id: params[:id])
+    @shipment_status = OrderDetail.find_by(id: params[:id]).shipment_status.shipment_status_name
   end
 end
