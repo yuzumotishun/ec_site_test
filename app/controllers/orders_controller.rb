359bc5737d8b9023264cc708196604cd6adf1480
hamabehayato <be_dash1122@icloud.com>
2021-05-21 20:40:29 +0900

    #30 @orders を @order (単数形) に変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
   def show
     @sum_price = 0
     @item_number = 1
-    @orders = Order.find_by(id: params[:id])
+    @order = Order.find_by(id: params[:id])
     @shipment_status = OrderDetail.find_by(id: params[:id]).shipment_status.shipment_status_name
   end
 end
