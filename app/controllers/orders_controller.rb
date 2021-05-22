2e4895aaaf4d07e48c395102569a02c3b4b5a157
hamabehayato <be_dash1122@icloud.com>
2021-05-22 16:30:13 +0900

    #30 shipment_status を参照するメソッドのモデルメソッド化

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,14 +4,5 @@ class OrdersController < ApplicationController
   def show
     @sum_price = 0
     @order = Order.find_by(id: params[:id])
-    shipment_statuses
   end
-
-  private
-   def shipment_statuses
-    @shipment_statuses =
-    @order.order_details.map do |order_detail|
-      order_detail.shipment_status.shipment_status_name
-    end
-   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,14 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details
+
+  def shipment_status
+    self.order_details.any? do |order_detail|
+      order_detail.shipment_status.shipment_status_name == "準備中"
+    end
+  end
+
+  def shipment_prepared?
+    shipment_status
+  end
 end
