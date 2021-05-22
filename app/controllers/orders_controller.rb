fcae41043591fce9e22c9c0620cb2dbb7afc93b4
hamabehayato <be_dash1122@icloud.com>
2021-05-22 18:10:19 +0900

    #30 @sum_price をモデルメソッド化

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
   before_action :correct_user, only: [:show]
 
   def show
-    @sum_price = 0
     @order = Order.find_by(id: params[:id])
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,14 @@ class Order < ApplicationRecord
       order_detail.shipment_status.shipment_status_name == "準備中"
     end
   end
+
+  def total_price
+    order_details.map do |order_detail|
+      order_detail.product.price * order_detail.order_quantity
+    end
+  end
+
+  def sum_price
+    total_price.sum
+  end
 end
