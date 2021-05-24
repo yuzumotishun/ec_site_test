535312e50c358e1b7b39264be2d52cbc21a3f657
shun <yuzumotishun@gmail.com>
2021-05-24 23:00:56 +0900

    shoulda_matchersの設定

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "" do
+    context "データが条件を満たすとき" do
+      it "保存できない" do
+        user = User.new(email: "test@example.com")
+        expect(user.valid?).to eq false
+      end
+    end
+
+    it { should validate_presence_of(:email) }
+  end
+end
