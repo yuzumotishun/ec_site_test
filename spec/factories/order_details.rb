54a5a769a305710330911c95f06b48967d2607bb
hamabehayato <be_dash1122@icloud.com>
2021-06-01 12:09:35 +0900

    #92 factory ファイルにて、親要素に当たる部分を{ nil } として追記

--- spec/factories/order_details.rb
+++ spec/factories/order_details.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :order_detail do
+    order { nil }
+    product { nil }
+    shipment_status { nil }
     order_detail_number { rand(64) }
     order_quantity { rand(1000) }
   end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :order do
+    user {nil}
     order_number { rand(99_999_999) }
   end
 end
