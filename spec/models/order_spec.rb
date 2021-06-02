2d51f32cb599a01261ab69bd57248fb621bc022a
hamabehayato <be_dash1122@icloud.com>
2021-06-02 10:02:21 +0900

    #92 shipment_status_name を直書きに変更

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -16,28 +16,17 @@ RSpec.describe Order, type: :model do
   end
 
   describe "shipment_status参照のテスト" do
-    let(:user_classification) { create(:user_classification, :customer) }
-    let(:user) { create(:user, user_classification: user_classification) }
-    let(:order) { create(:order, user: user) }
-    let(:category) { create(:category) }
-    let(:sale_status) { create(:sale_status) }
-    let(:product_status) { create(:product_status) }
-    let(:product) { create(:product, user: user, category: category, sale_status: sale_status, product_status: product_status) }
-    let(:shipment_status) { create(:shipment_status, :preparation) }
-    let(:order_detail) { create(:order_detail, order: order, shipment_status: shipment_status, product: product) }
+    let(:shipment_status) { create(:shipment_status, shipment_status_name: "準備中") }
     context "order_details.shipment_status が 準備中 のとき" do
       it "true が返される" do
-        expect(order_detail.shipment_status.shipment_status_name).to eq "準備中"
+        expect(shipment_status.shipment_status_name).to eq "準備中"
       end
     end
 
     context "order_details.shipment_status に 発送済 が含まれるとき" do
-      let(:shipment_status_shipped) { create(:shipment_status, :shipped) }
-      let(:order_detail_shipped) { create(:order_detail, order: order, shipment_status: shipment_status_shipped, product: product) }
+      let(:shipment_status_shipped) { create(:shipment_status, shipment_status_name: "発送済") }
       it "false が返される" do
-        preparation = order_detail.shipment_status.shipment_status_name
-        shipped = order_detail_shipped.shipment_status.shipment_status_name
-        expect(preparation && shipped).not_to eq "準備中"
+        expect(shipment_status && shipment_status_shipped).not_to eq "準備中"
       end
     end
   end
