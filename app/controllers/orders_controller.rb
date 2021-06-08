03658939b7036138e29a9741f0a13140d75d467b
yuzumotishun <yuzumotishun@gmail.com>
2021-06-08 19:53:15 +0900

    #62 注文番号生成ロジックの修正と、session[:cart]の中身の有無による条件分岐を追記しました。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,18 +5,23 @@ class OrdersController < ApplicationController
     @order = Order.find_by(id: params[:id])
   end
 
+  #### TODO:カートに商品が入ってる状態で当該商品を消し、カートに入っている商品すべて存在しないものにしてから注文ボタンを押すした際に中身のないOrderが作成されることを防ぐ ####
   # カート内商品情報を保存する
   def create
+    if session[:cart].blank?
+      return redirect_to carts_show_path
+    end
+    complex_data = current_user.id.to_s + Time.current.to_i.to_s
     order = current_user.orders.create(
       order_date: Time.current,
-      order_number: ("%016d" % (current_user.id + Time.current.to_i)),
+      order_number: complex_data.rjust(16, "0")
     )
     session[:cart].each do |cart|
       order.order_details.create(
         product_id: cart["product_id"],
         shipment_status_id: 1,
-        order_detail_number: (current_user.id + cart["product_id"].to_i + Time.current.to_i),
-        order_quantity: cart["quantity"],
+        order_detail_number: cart["product_id"] + complex_data,
+        order_quantity: cart["quantity"]
       )
     end
     session[:cart].clear
