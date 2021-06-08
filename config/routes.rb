06e6b2bc1a31ad6ce469e43c65fe372fbb15c715
yuzumotishun <yuzumotishun@gmail.com>
2021-06-08 23:11:26 +0900

    #62 routesのorder#createにあたる記述を消し、それに対応していたルーティングを変更しました。

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   post 'carts/add_cart', to: 'carts#add_cart'
   patch 'carts/change_quantity', to: 'carts#change_quantity', as: 'change_item_quantity'
   delete 'carts/destroy_carts_item', to: 'carts#destroy_carts_item', as: 'destroy_carts_item'
-  post 'orders/create', to: 'orders#create', as: 'orders_create'
   get '/perchase_completed/:id', to: 'orders#perchase_completed', as: 'perchase_completed'
   resources :products
   resources :users
