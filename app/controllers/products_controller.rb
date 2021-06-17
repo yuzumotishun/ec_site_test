d1d9a3785c93403fc04ebfa9ae701adece8cbd6d
たくろう <taku-calcio@ezweb.ne.jp>
2021-06-18 08:30:36 +0900

    無駄なコメントアウトの削除

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,15 +7,3 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
   end
 end
-
-# <div class="container">
-#       <nav aria-label="...">
-#         <ul class="pagination justify-content-center">
-#           <li class="page-item active">
-#             <a class="page-link" href="#">1<span class="sr-only">(current)</span></a>
-#           </li>
-#           <li class="page-item"><a class="page-link" href="#">2</a></li>
-#           <li class="page-item"><a class="page-link" href="#">3</a></li>
-#         </ul>
-#       </nav>
-#     </div>
